a6eeaf2d5827d47d903571cb804a62735a4a053b
Josh Hartley <me@jhartley.net>
2024-01-28 21:22:52 +0000

    add main layout

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,8 @@ module.exports = function(eleventyConfig) {
     return {
         dir: {
             input: "content",
+            includes: "../_includes",
+            data: "../_data",
             output: "_site" // Github Pages
         }
     }
