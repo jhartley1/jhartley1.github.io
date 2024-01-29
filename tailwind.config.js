b0214775faa9928b5d095b111140ce4d54070eb4
Josh Hartley <me@jhartley.net>
2024-01-29 14:17:24 +0000

    switch to nunjucks

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,liquid}"],
+  content: ["./src/**/*.{html,liquid,njk}"],
   theme: {
     extend: {},
   },
